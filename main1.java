293873c097fb8e14794a26e0949e7cfbb2f1c31f
Jit Chakraborty <110622733+jit-12345@users.noreply.github.com>
2023-01-03 17:39:05 +0530

    quicksort added

--- main1.java
+++ main1.java
@@ -1,6 +1,11 @@
 public class main1{
-    public static void main(String[] args) {
+    public static void quicksort()
+    {
+        System.out.println("This is a new branch");
+    }
+    public static void main(String[] args) 
+    {
     System.out.println("Hello World!");
     System.out.println("My name is Jit Chakraborty");
-}
+    }
 }
